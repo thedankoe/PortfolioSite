428d9e1483924b15ffbdb398a3e0dc3c4d7e3cab
Daniel Koenigslieb <Daniel@Daniels-MacBook-Pro.local>
2018-04-24 09:46:00 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("Server has started");
+// });
+
+app.listen(3000, function() {
+  console.log("server started");
+});
---
+++ public/js/theDanKoe.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	//NAV SCROLL ANIM
+	$(window).on("scroll", function() {
+		if($(document).scrollTop() > 550) {
+			$("nav").addClass("navBlack");
+		} else {
+			$("nav").removeClass("navBlack");
+		}
+	});
+
+	//ABOUT ME SCROLL ANIM
+	$("a[href^='#']").on("click", function(event) {
+		event.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		//SCROLL AND SHOW HASH
+		$("html, body").animate( {
+			"scrollTop": $target.offset().top
+		}, 700, "swing", function() {
+			window.location.hash = target;
+		});
+	});
+
+	$(window).on("scroll", function() {
+		if ($(window).width() <= 549){
+	    if($(document).scrollTop() > 1180){
+	        skillbarAnimation();
+	    }
+		} else if ($(window).width() > 549 && $(window).width() <= 991) {
+	    if ($(document).scrollTop() > 790) {
+	        skillbarAnimation();
+	    }
+		} else {
+	    if ($(document).scrollTop() > 760) {
+	        skillbarAnimation();
+	    }
+	}
+	});
+
+	// skillbar animation
+	var skillbarAnimation = function() {
+		$('.skillbar').each(function() {
+		$(this).find('.skillbar-bar').animate( {
+			width:$(this).attr('data-percent')
+		}, 2500);
+	});
+}
+
+});
